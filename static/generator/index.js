f5439d3af3a95e488961680bae147b93e7384f0a
0xDas <126429597+Callmedas69@users.noreply.github.com>
2024-08-03 17:59:39 +0700

    path

--- static/generator/index.js
+++ static/generator/index.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("meme-canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 const fistImageTemplate = new Image();
-fistImageTemplate.src = "/fist.png";
+fistImageTemplate.src = "./fist.png";
 fistImageTemplate.crossOrigin = "anonymous";
 const laserImageTemplate = new Image();
-laserImageTemplate.src = "/laser.png";
+laserImageTemplate.src = "./laser.png";
 laserImageTemplate.crossOrigin = "anonymous";
 
 let canvasImage = new Image();
